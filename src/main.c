5c66c3ee27e75d3284152c03a7aacf1bb4f70763
Matt Davies <mattie.davies@gmail.com>
2017-12-07 17:01:07 -0500

    Updated ref to kore.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,15 @@
 
 int kmain(int argc, char** argv)
 {
+    Pool(i64) p = 0;
+
+    i64 i0 = poolAcquire(p);
+    i64 i1 = poolAcquire(p);
+    i64 i2 = poolAcquire(p);
+
+    poolRecycle(p, i1);
+    i64 i3 = poolAcquire(p);
+
     return 0;
 }
 
